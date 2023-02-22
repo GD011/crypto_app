a47dc550a733766ae57686fde718d6ebeb3842ec
GD011 <115717261+GD011@users.noreply.github.com>
2023-02-22 16:27:22 +0530

    Second comit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export{default as Navbar } from "./navbar";
-export{default as Homepage} from "./homepage";
-export{default as Exchanges} from "./exchange";
-export{default as Cryptocurrencies} from "./cryptocurrencies"
-export{default as  CryptoDetails} from "./cryptoDetails"
-export{default as News } from "./News"
+export { default as Homepage } from './Homepage';
+export { default as Navbar } from './Navbar';
+export { default as News } from './News';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as Exchanges } from './Exchanges';
+export { default as CryptoDetails } from './CryptoDetails';
